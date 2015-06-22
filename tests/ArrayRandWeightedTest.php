d7569e01cc8d2b75cc559ed89df210b355955c2e
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2015-06-22 09:35:33 +0200

    array_rand_weighted; cs fix

---
+++ tests/ArrayRandWeightedTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Spatie\Test;
+
+use function spatie\array_rand_weighted;
+
+class ArrayRandWeightedTest extends \PHPUnit_Framework_TestCase
+{
+    protected $testArray = [
+        'foo' => 1,
+        'bar' => 1,
+        'baz' => 4,
+    ];
+
+    /**
+     * @test
+     */
+    public function it_can_handle_an_empty_array()
+    {
+        $this->assertNull(array_rand_weighted([]));
+    }
+
+    /**
+     * @test
+     */
+    public function it_retrieves_a_value_from_the_array()
+    {
+        $this->assertContains(array_rand_weighted($this->testArray), array_keys($this->testArray));
+    }
+
+    /**
+     * @test
+     */
+    public function it_is_safe_to_assume_the_result_is_weighted()
+    {
+        $results = [];
+
+        for ($i = 0; $i < 100; ++$i) {
+            $results[] = array_rand_weighted($this->testArray);
+        }
+
+        $resultCounts = array_count_values($results);
+
+        $this->assertGreaterThan($resultCounts['foo'] + $resultCounts['bar'], $resultCounts['baz']);
+    }
+}
